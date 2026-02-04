898bcfdf0d97934e081e1d2bcc05b42e0f7f00f6
Ljubica Vlahović <vlahovic.ljubica5@gmail.com>
2026-02-04 18:10:50 +0100

    Button komponenta

--- frontend/src/pages/AddEvent.jsx
+++ frontend/src/pages/AddEvent.jsx
@@ -1,10 +1,16 @@
+import Button from "../components/Button";
+
 function AddEvent() {
   return (
     <div>
       <h2>Dodaj događaj</h2>
-      <p>Forma za dodavanje novih događaja u kalendar.</p>
+        <p>Forma za dodavanje novih događaja u kalendar.</p>
+      <Button text="Sačuvaj" onClick={() => alert("Sačuvano")} />
+      <Button text="Otkaži" onClick={() => alert("Otkazano")} />
     </div>
   );
 }
 
 export default AddEvent;
+
+
